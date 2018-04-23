3b7359d570731394932192cbacc66c54ecf151ef
Sam Wellander <samwellander@gmail.com>
2018-04-23 19:44:44 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,7 @@
+function theBeatlesPlay(musicians, instruments) {
+  var output = [];
+  for (i = 0; i < musicians.length; i++) {
+    var sentence = musician[i] + ' plays the ' + instruments[i];
+    output.push(sentence);
+  }
+}
